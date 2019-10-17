ea10c534b6ac3922dc767c4c4aca240187d7f2ee
wty625073563 <1245183029@qq.com>
2019-10-17 23:01:17 +0800

    feat: add eslint subcommand

--- src/install.js
+++ src/install.js
@@ -1,31 +1,48 @@
 #!/usr/bin/env node
-const util = require('util')
-const exec = util.promisify(require('child_process').exec)
 const program = require('commander')
-const chalk = require('chalk')
+const spawn = require('cross-spawn')
+const helper = require('./helper')
+const eslint = require('./feature/eslint-prettier/eslint')
+const prettier = require('./feature/eslint-prettier/prettier')
 
-async function linearShell(commands) {
-  for(let i = 0; i < commands.length; i++){
-    let command = commands[i]
-    console.log(chalk.green(`excute ${ command } ......`))
-    const { stdout, stderr } = await exec(`powershell.exe ${ command }`)
-    console.log('stdout:', stdout)
-    stderr && console.log('stderr:', stderr)
-  }
+function exec(command, args, isSync = false){
+  return (isSync ? spawn.sync : spawn)(command, args, { stdio: 'inherit'})
 }
 
+function retry(commands){
+  for(let i = 0; i < commands.length; i++){
+    let command = commands[i].split(' ')
+    if(!exec(command[0], command.slice(1), true).error) return
+  }
+}
 
 program
   .version('0.0.1', '-v, --vers', 'output the current version')
   .command('eslint')
-  .option('-q, --query')
+  .option('-q, --query', 'specify your style through answer question')
+  .option('-s, --save', 'save your prefer eslint style')
   .action((opt) => {
+    if(opt.save){
+      eslint.savePrefer()
+      return 
+    }
+
     if(opt.query){
-      linearShell([
-        'yarn add eslint --dev',
-        'npx eslint --init'
+      retry([
+        'npm install eslint --save-dev',
+        'yarn add eslint --dev'
+      ])
+      exec('npx', ['eslint', '--init'], true)
+    }else {
+      const { devDependencies: eslintDepend } = helper.install(opt, eslint)
+      const { devDependencies: prettierDepen } = helper.install(opt, prettier)
+      const dependencies = eslintDepend.concat(prettierDepen).join(' ')
+      retry([
+        `yarn add ${ dependencies} --dev`,
+        `npm install ${ dependencies } --save-dev`
       ])
     }
+
   })
 
 program.parse(process.argv)
