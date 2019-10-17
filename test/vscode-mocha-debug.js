bf3019ceadd36a67522df4ac5deedfcb193fa6c9
jelee <1245183029@qq.com>
2019-10-17 19:06:32 +0800

    fix: leave over the .vscode dir when test test/vscode-mocha-debug

--- test/vscode-mocha-debug.js
+++ test/vscode-mocha-debug.js
@@ -2,6 +2,8 @@ const vsdebug = require('../src/feature/mocha/vscode-debug')
 const helper = require('../src/helper')
 const chai = require('chai')
 const expect = chai.expect
+const fsync = require('fs-sync')
+const path = require('path')
 
 describe('#mocha-vs-debug', function(){
   it('#mocha-vs-debug', function(done){
@@ -10,6 +12,7 @@ describe('#mocha-vs-debug', function(){
     const file = vsdebug.install({ file: name}, root)
     const json = helper.getJSON(file)
     expect(json.configurations.some(c => c.name === name)).to.be.true
+    fsync.remove(path.join(__dirname, '/.vscode'))
     done()
   })
 })
