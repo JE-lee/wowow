c75e35d978e3ec91c3b38b9e024078d9a5592767
jelee <1245183029@qq.com>
2019-10-18 19:43:16 +0800

    feat: add commitlin install script

--- src/install.js
+++ src/install.js
@@ -10,6 +10,9 @@ const eslint = require('./feature/eslint-prettier/eslint')
 const prettier = require('./feature/eslint-prettier/prettier')
 const mocha = require('./feature/mocha/index')
 const debugMocha = require('./feature/mocha/vscode-debug')
+const commitLint = require('./feature/commitlint/index')
+
+const bin = 'lalala'
 
 function exec(command, args, isSync = false){
   return (isSync ? spawn.sync : spawn)(command, args, { stdio: 'inherit'})
@@ -31,6 +34,49 @@ function installDependencies(dependencies){
   ])
 }
 
+function hasGitRepos(){
+  return fsync.isDir(path.join(process.cwd(), '/.git'))
+}
+
+function installEslint(opt){
+  const { devDependencies: eslintDepend } = helper.install(opt, eslint)
+  const { devDependencies: prettierDepen } = helper.install(opt, prettier)
+  const dependencies = eslintDepend.concat(prettierDepen)
+  installDependencies(dependencies)
+  // no .git
+  if(!hasGitRepos()){
+    console.log(chalk.red(`
+      you shoulu init your git repository and then run 
+      npx mrm lin-staged
+    `))
+    return
+  }else {
+    // pre-commit
+    exec('npx', ['mrm', 'lint-staged'], true)
+  }
+}
+
+// should install husky first
+function installCommitLint(opt){
+  // if no git repos
+  if(!hasGitRepos()){
+    console.log(chalk.red(`
+      you shoulu init your git repository and then run 
+      ${ bin } commitlint
+    `))
+    return
+  }
+  const { devDependencies } = helper.install(opt, commitLint)
+  installDependencies(devDependencies)
+  // write husky hook
+  const root = process.cwd()
+  const file = path.join(root, '/package.json')
+  const packageJson = helper.getJSON(file)
+  packageJson.husky = packageJson.husky || {}
+  packageJson.husky.hooks = packageJson.husky.hook || {}
+  packageJson.husky.hooks['commit-msg'] = 'commitlint -E HUSKY_GIT_PARAMS'
+}
+
 // eslint
 program
   .version('0.0.1', '-v, --vers', 'output the current version')
@@ -41,23 +87,8 @@ program
       eslint.savePrefer()
       return 
     }
-
-    const { devDependencies: eslintDepend } = helper.install(opt, eslint)
-    const { devDependencies: prettierDepen } = helper.install(opt, prettier)
-    const dependencies = eslintDepend.concat(prettierDepen)
-    installDependencies(dependencies)
-    // no .git
-    if(!fsync.isDir(path.join(process.cwd(), '/.git'))){
-      console.log(chalk.red(`
-        you shoulu init your git repository and then run 
-        npx mrm lin-staged
-      `))
-      return
-    }else {
-      // pre-commit
-      exec('npx', ['mrm', 'lint-staged'], true)
-    }
-    
+    installEslint(opt)
+    installCommitLint(opt)
   })
 
 // mocha
