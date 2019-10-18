4c94e2b8078eb6caa3ee442e906bb6c32e559d58
jelee <1245183029@qq.com>
2019-10-18 18:14:31 +0800

    feat: add commitlint

---
+++ src/feature/commitlint/index.js
@@ -0,0 +1,12 @@
+exports.install = function(){
+  return {
+    config: {
+      extends: ['@commitlint/config-conventional'],
+      rules: {
+        // Place your rules here
+      },
+    },
+    filename: '.commitlintrc.json',
+    devDependencies: ['@commitlint/config-conventional', '@commitlint/cli']
+  }
+}
