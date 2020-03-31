23bd4c7566d467182a810a3ddb7fc1f38b13dbd0
jelee <1245183029@qq.com>
2020-03-31 15:03:21 +0800

    refactor: 移除多余的测试文件

--- test/commitizen.js
+++
@@ -1,4 +0,0 @@
-const helper = require('../test-helper')
-const commitizen = require('../src/feature/commitizen/index')
-
-helper.install(commitizen, 'commitizen')
--- test/eslint.js
+++
@@ -1,4 +0,0 @@
-const helper = require('../test-helper')
-const prettier = require('../src/feature/eslint-prettier/eslint')
-
-helper.install(prettier, 'eslint')
